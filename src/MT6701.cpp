b9dda1fb6676c7ed65a46d6ae4d0a811c5535691
Noran Raskin <dev@noranraskin.com>
2023-12-28 16:00:39 +0100

    Add accumulated count, background task for ESP32 and documentation

--- src/MT6701.cpp
+++ src/MT6701.cpp
@@ -1,20 +1,87 @@
+#include <Wire.h>
+// Imports for ESP32
+#ifdef ESP32
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#endif
 #include "MT6701.hpp"
 
+/**
+ * @brief Constructs an MT6701 encoder object.
+ *
+ * @param device_address I2C address of the MT6701.
+ * @param update_interval Interval in milliseconds at which to update the encoder count.
+ */
+MT6701::MT6701(uint8_t device_address, int update_interval)
+    : address(device_address), updateIntervalMillis(update_interval)
+{
+}
+
+/**
+ * @brief Initialises the MT6701 encoder.
+ * @note This function must be called before any other MT6701 functions.
+ * @note If hardware permits, this function starts a background task to update
+ *       the encoder count at regular intervals.
+ */
 void MT6701::begin()
 {
     Wire.begin();
+    Wire.setClock(400000);
+#ifdef ESP32
+    xTaskCreatePinnedToCore(updateTask, "MT6701 update task", 2048, this, 1, NULL, 1);
+#endif
 }
 
+/**
+ * @brief Returns the shaft angle of the encoder in radians.
+ *
+ * @return Angle in radians withing the range [0, 2*PI).
+ */
 float MT6701::getAngleRadians()
 {
     int count = getCount();
     return count * COUNTS_TO_RADIANS;
 }
 
+/**
+ * @brief Returns the shaft angle of the encoder in degrees.
+ *
+ * @return Angle in degrees withing the range [0, 360).
+ */
 float MT6701::getAngleDegrees()
 {
     int count = getCount();
     return count * COUNTS_TO_DEGREES;
+}
+
+/**
+ * @brief Returns the accumulated number of full turns of the encoder shaft since initialisation.
+ *
+ * @return Number of full turns.
+ */
+int MT6701::getFullTurns()
+{
+    return accumulator / COUNTS_PER_REVOLUTION;
+}
+
+/**
+ * @brief Returns the accumulated number of turns of the encoder shaft since initialisation as a float.
+ *
+ * @return Number of turns.
+ */
+float MT6701::getTurns()
+{
+    return (float)accumulator / float(COUNTS_PER_REVOLUTION);
+}
+
+/**
+ * @brief Returns the accumulated count the encoder has generated since initialisation.
+ *
+ * @return Raw accumulator value.
+ */
+int MT6701::getAccumulator()
+{
+    return accumulator;
 }
 
 int MT6701::getCount()
@@ -35,4 +102,30 @@ int MT6701::getCount()
         angle_l = Wire.read() >> 2;
 
     return (int)((angle_h << 6) | angle_l);
+}
+
+void MT6701::updateCount()
+{
+    int newCount = getCount();
+    int diff = newCount - count;
+    if (diff > COUNTS_PER_REVOLUTION / 2)
+    {
+        diff -= COUNTS_PER_REVOLUTION;
+    }
+    else if (diff < -COUNTS_PER_REVOLUTION / 2)
+    {
+        diff += COUNTS_PER_REVOLUTION;
+    }
+    accumulator += diff;
+    count = newCount;
+}
+
+void MT6701::updateTask(void *pvParameters)
+{
+    MT6701 *mt6701 = static_cast<MT6701 *>(pvParameters);
+    while (true)
+    {
+        mt6701->updateCount();
+        vTaskDelay(pdMS_TO_TICKS(mt6701->updateIntervalMillis));
+    }
 }
--- src/MT6701.hpp
+++ src/MT6701.hpp
@@ -1,4 +1,5 @@
-#include <Wire.h>
+#pragma once
+#include <atomic>
 #ifndef PI
 #define PI 3.1415926535897932384626433832795
 #endif
@@ -11,13 +12,23 @@ public:
     static constexpr float COUNTS_TO_RADIANS = 2.0 * PI / COUNTS_PER_REVOLUTION;
     static constexpr float COUNTS_TO_DEGREES = 360.0 / COUNTS_PER_REVOLUTION;
 
-    MT6701(uint8_t device_address = DEFAULT_ADDRESS) : address(device_address) {}
-
+    MT6701(uint8_t device_address = DEFAULT_ADDRESS, int update_interval = 10);
     void begin();
     float getAngleRadians();
     float getAngleDegrees();
+    int getFullTurns();
+    float getTurns();
+    int getAccumulator();
 
 private:
     uint8_t address;
+    int updateIntervalMillis;
+    std::atomic<int> count{0};
+    std::atomic<int> accumulator{0};
+
     int getCount();
+    void updateCount();
+#ifdef ESP32
+    static void updateTask(void *pvParameters);
+#endif
 };
